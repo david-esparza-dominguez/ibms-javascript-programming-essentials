bb6c19402454f027838c27008739275f13ac874d
David Esparza Dominguez <82722961+david-esparza-dominguez@users.noreply.github.com>
2026-04-25 10:40:07 -0700

    refactor scope_lab.js to demonstrate variable scoping and reassignment within blocks

--- SampleFolder/Scope/scope_lab.js
+++ SampleFolder/Scope/scope_lab.js
@@ -1,4 +1,50 @@
-// Global Scope
+// ==================================================
+// Step 1 + Step 2
+// Create block, declare variables + reassign inside
+// ==================================================
+"use strict";
+{
+    var blockVar = "Var 1st Value";
+    let blockLet = "Let 1st Value";
+    const blockConst = "Const 1st Value";
+
+    // Reassignment inside block
+    //blockVar = "Var 2nd Value"; // Works
+    //blockLet = "Let 2nd Value"; //works
+    //blockConst = "Const 2nd Value"; // TypeError: Assignment to constant variable.
+
+    console.log(blockVar);
+    console.log(blockLet);
+    console.log(blockConst);
+}
+
+    // ==================================
+    // Step 3
+    // Testing outside the block
+    // ==================================
+
+    console.log(blockVar); // is accesible outside the block
+    //console.log(blockLet); // ReferenceError: blockLet is not defined is not accessible outside the block
+    //console.log(blockConst) // ReferenceError: blockConst is not defined is not accessible outside the block
+
+    // ================================
+    // STEP 3 (continued)
+    // Try reassignment outside the box
+    // ================================
+    
+    blockVar = "Var changed outside the box"; // works outside the box
+    console.log(blockVar); // worked outside the box
+
+    //blockLet = "Let: Changed Outside"; // was working at first which was very confusing. Had to "use strict" mode to get the correct behavior. without it a javascript takes an undeclared variable and creates a global variable. 
+    //console.log(blockLet); //ReferenceError: blockLet is not defined
+
+    blockConst = "Const: changed outside"; // without "strict mode" it will create a variable instead of attempt to modify one. 
+    console.log(blockConst) // ReferenceError: blockConst is not defined
+
+/* 
+COMMENTED EVERYTHING OUT TO WORK ON PRACTICE TASKS AT THE END
+
+Global Scope
 var globalVar = "I'm a global variable";
 let globalLet = "I'm also global, but scoped with let";
 const globalConst = "I'm a global constant";
@@ -21,6 +67,7 @@ console.log(globalConst); // Output: "I'm a global constant"
 //console.log(blockLet);
 //console.log(blockConst);
 
+/*
 function show(){
 var functionVar = "I'm a block-scoped var";
 let functionLet = "I'm a block-scoped let";
@@ -31,3 +78,7 @@ show();
 console.log(functionVar); // Throws ReferenceError
 console.log(functionLet); // Throws ReferenceError
 console.log(functionConst); // Throws ReferenceError
+
+COMMENTED EVERYTHING ELSE OUT TO WORK ON PRACTICE TASKS AT THE END
+*/ 
+
